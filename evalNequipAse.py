eb200d04843d9a2f3ec106ba1c27a61b8073e22b
Dominic Welti <weltidom@gmail.com>
2024-02-01 16:51:38 +0100

    evalNequip added

---
+++ evalNequipAse.py
@@ -0,0 +1,28 @@
+from ase.io import read, write
+import numpy as np
+
+from nequip.ase import NequIPCalculator
+
+
+splits=np.asplits(0,10)
+
+for split in splits:
+    # paths to
+    model = f'results/model_{split}.pth' # needs to be a deployed model
+    data = f'/home/st/st_us-031400/st_st179390/traj/hea/valid/valid_{split}.xyz'
+    output = f'eval/standard/test_{split}.xyz'
+
+
+    calculator = NequIPCalculator.from_deployed_model(model_path=model)
+    atoms_lst = read(data, index=':', format='extxyz')
+
+    for atoms in atoms_lst: 
+        atoms.set_calculator(calculator)
+        e=atoms.get_total_energy()
+        f=atoms.get_forces()
+        atoms.calc=None
+        atoms.info['NequIP energy'] = e
+        atoms.arrays['NequIP forces'] = f
+
+    write(output, images=atoms_lst, format='extxyz')
+    print(f"Split {split} evaluation finished")
--- pyVASPFakeML_AB/README.md
+++ pyVASPFakeML_AB/README.md
@@ -1,1 +1,3 @@
-test
+# VASP output to ML_AB
+
+Produce ML_AB file utilized by VASP machine learning force fields ([MLFF](https://www.vasp.at/wiki/index.php/Machine_learning_force_field_calculations:_Basics)). Using this, ab-initio data calculated without MLFF enabled can still be used to obtain a force field by setting `ML_MODE = select`.
